a7edc7086f1a155e84a8910b25f6b6d685e02ed5
Fuad Hasan <fuadhasan3503@gmail.com>
2025-08-17 18:38:29 +0600

    update error page

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { router } from "./routes/Routes";
 import AuthProvider from "./providers/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "./context/ThemeContext"; // New import
+import { ThemeProvider } from "./context/ThemeContext"; 
 
 // Create a client
 const queryClient = new QueryClient();
